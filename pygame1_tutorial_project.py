3982b62cd467c2ac530bcee7042bde7a44946b7f
Logan Hentschel <hentscht@student.psdr3.org>
2020-05-13 15:44:48 -0500

    First tutorial video done & ergo first section finished; screen will apear and not suddenly blip out of existance, character is able to move around in all 4 directions (even diagonally toogit add .), and there are no longer rectangle lines being drawn as the character moves (although that could be used as a drawing game in the future)

--- pygame1_tutorial_project.py
+++ pygame1_tutorial_project.py
@@ -9,20 +9,41 @@ pygame.display.set_caption("First Game")
 #main variables
 x = 50
 y = 50
-wdith = 40
+width = 40
 height = 60
-velocity = 5
+vel = 5 #velocity; how far the character moves in any direction
 
 # # #
 #main loops
 run = True
-
 while run:
     pygame.time.delay(100) #creathes a sort of clock; also makes things have a delay so they dont happen instantainiously; 100 miliseconds
-
+# # # # #
     for event in pygame.event.get(): #creates/gets a list of all the events that happen
         if event.type == pygame.QUIT: #if the big red button in the corner is pushed...
             run = False
+# # #
+#list of...
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+    win.fill((0,0,0))
 
+# # #
+    #draws rectangle; other shapes have their code on the pygame website 
+    #everything in python is a surface, paranthases variables are the surfaces the rectangle is put on, then RGB, then the rect (defining its dimensions)
+    pygame.draw.rect(win, (0, 255, 0), (x, y, width, height)) 
+    pygame.display.update() #gets the rect to actually display on the screen [green rectangle located at 50,50, with a wdith of 40 and aheight of 60]
+
+
+
+
+# # # # # # # 
 pygame.quit()
-
