e4b27221075d90efc08cf8ee67db3264bb6a335b
Logan Hentschel <hentscht@student.psdr3.org>
2020-05-13 15:12:48 -0500

    Following tutorial; some issues getting the window to pop up, unsure of what is causing the issue. Will see if problem continues as more code is added

---
+++ pygame1_tutorial_project.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+# # # # #
+#setting up gaming window; parenthases variables are height/width
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("First Game")
