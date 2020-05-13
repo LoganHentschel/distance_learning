a6cd4f9a0792e0cd0e9375a1282fbdae770ca1e6
Logan Hentschel <hentscht@student.psdr3.org>
2020-05-13 15:21:44 -0500

    Previous issue where screen would not show up has been fixed, though what caused that previous error is still unknown --- code now has the functionability to exit completely when the red x in the top corner is pressed. if code executing the function is taken away, it breaks and turns into a spinny beach ball of death

--- pygame1_tutorial_project.py
+++ pygame1_tutorial_project.py
@@ -3,5 +3,26 @@ pygame.init()
 # # # # #
 #setting up gaming window; parenthases variables are height/width
 win = pygame.display.set_mode((500, 500))
-
 pygame.display.set_caption("First Game")
+
+# # #
+#main variables
+x = 50
+y = 50
+wdith = 40
+height = 60
+velocity = 5
+
+# # #
+#main loops
+run = True
+
+while run:
+    pygame.time.delay(100) #creathes a sort of clock; also makes things have a delay so they dont happen instantainiously; 100 miliseconds
+
+    for event in pygame.event.get(): #creates/gets a list of all the events that happen
+        if event.type == pygame.QUIT: #if the big red button in the corner is pushed...
+            run = False
+
+pygame.quit()
+
