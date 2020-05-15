a80d10840d76a04f0e2420747de1adf87d64d605
Logan Hentschel <hentscht@student.psdr3.org>
2020-05-14 20:08:10 -0500

    Currently creating code in order to create the new sprite & implement the sprite pictures and background. Since basic code is being moved/deleted, another file was created to house the base code that was moved/removed. The current game is more of a platformer since the up/down movement was taken out. Functions have been created to 'draw' the character sprite and background

--- tutorial_project/pygame1_tutorial_project.py
+++ tutorial_project/pygame1_tutorial_project.py
@@ -7,19 +7,42 @@ pygame.display.set_caption("First Game")
 #
 screenwidth = 500
 screenheight = 500
-
+# #
+#puts all the image sprites into python - pasted because many images is tedious 
+#**It is possible to simply flip the images in reguards to right & left; both are separate in this case
+#use ' pygame.path.join ('folder_name', sprite_name') ' to get images from different directories [ or 'folder/'sprite name' ?]
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+#omg theres so many whyyyy... lukcily only had to paste it this time lol. going to be soo tedious when I have to do it myself
 # # #
 #main variables
 x = 50
 y = 50
-width = 40
-height = 60
+width = 64
+height = 64
 vel = 5 #velocity; how far the character moves in any direction
-
+#
+#jump variables
 isJump = False
 JumpCount = 10
+#
+#animaion variables
+left = False
+right = False
+walk_count = 0
 
-# # #
+# # # # 
+#functions!
+def  redrawGameWindow():
+    global walk_count
+
+    win.blit(bg, (0,0))
+    pygame.draw.rect(win, (0, 255, 0), (x, y, width, height)) 
+    pygame.display.update() 
+
+# # # # # # # 
 #main loops
 run = True
 while run:
@@ -37,10 +60,6 @@ while run:
     if keys[pygame.K_RIGHT] and x < (screenwidth-width): #'-vel' ?
         x += vel
     if not  (isJump) :
-        if keys[pygame.K_UP] and y > vel:
-            y -= vel
-        if keys[pygame.K_DOWN] and y < (screenheight-height): # -vel?
-            y += vel
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
@@ -53,15 +72,10 @@ while run:
         else:
             isJump = False
             JumpCount = 10
-
-    # # #
-    win.fill((0,0,0))
-
+            
 # # #
-    #draws rectangle; other shapes have their code on the pygame website 
-    #everything in python is a surface, paranthases variables are the surfaces the rectangle is put on, then RGB, then the rect (defining its dimensions)
-    pygame.draw.rect(win, (0, 255, 0), (x, y, width, height)) 
-    pygame.display.update() #gets the rect to actually display on the screen [green rectangle located at 50,50, with a wdith of 40 and aheight of 60]
+#Call functions at end!
+redrawGameWindow()
 
-# # # # # # # 
+# # # # # # #
 pygame.quit()
