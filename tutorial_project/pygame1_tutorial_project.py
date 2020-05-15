8f4f6ac47937d5d74c106a49c3e0fc897e316f49
Logan Hentschel <hentscht@student.psdr3.org>
2020-05-15 00:08:39 -0500

    Section 4 completed! This section did not add any new features to the program, but instead went through how to fix up the code & the tutorial explained why exactly the code would need to be fixed up in the first place - smaller games don;t have to go through the class system, but for bigger games that would have a lot of variables, it's much mroe recomended. SO the variables and code were fixed up, with the player character being moved into it;s own class. I'll agree it is kinda tedious to have to put 'character.' in front of every single variable, but hey you do what you gotta do. -- I also removed all comments (except the one that explains how the fixing is optional for small games) to help clean up the code & organized the # dividers to make the code look cleaner. It was interesting to learn about the class/object optimization, though I don;t 100% understand it; I'll be looking more into it later on!

--- tutorial_project/pygame1_tutorial_project.py
+++ tutorial_project/pygame1_tutorial_project.py
@@ -1,109 +1,103 @@
 import pygame
 pygame.init()
-# # # # #
-#setting up gaming window; parenthases variables are height/width
+# # # # # # #
+# # # # # # # 
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
 #
 screenwidth = 500
 screenheight = 500
 # #
-#puts all the image sprites into python - pasted because many images is tedious 
-#**It is possible to simply flip the images in reguards to right & left; both are separate in this case
-#use ' pygame.path.join ('folder_name', sprite_name') ' to get images from different directories [ or 'folder/'sprite name' ?]
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 bg = pygame.image.load('bg.jpg')
 char = pygame.image.load('standing.png')
-#omg theres so many whyyyy... lukcily only had to paste it this time lol. going to be soo tedious when I have to do it myself
 
 clock = pygame.time.Clock()
 
-
-# # #
-#main variables
-x = 225
-y = 400
-width = 64
-height = 64
-vel = 5 #velocity; how far the character moves in any direction
-#
-#jump variables
-isJump = False
-JumpCount = 10
-#
-#animaion variables
-left = False
-right = False
-walk_count = 0
-
-# # # # 
+# # # # #
+# # # # #
+#This is optional; for small/simple games you don't have to make these classes, but for bigger, more complex games... its much more recomended
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        #
+        self.vel = 5
+        self.isJump = False
+        self.JumpCount = 10
+        #
+        self.left = False
+        self.right = False
+        self.walk_count = 0
+# #
+    def draw(self, win):
+        if self.walk_count+1 >= 27:
+            self.walk_count = 0
+        #
+        if self.left:
+            win.blit(walkLeft[self.walk_count//3], (self.x,self.y))
+            self.walk_count += 1
+        elif self.right:
+            win.blit(walkRight[self.walk_count//3], (self.x,self.y))
+            self.walk_count += 1
+        else:
+            win.blit(char, (self.x,self.y))
+# # # # #
+# # # # #
 #functions!
 def  redrawGameWindow():
-    global walk_count
-
     win.blit(bg, (0,0))
-
-    if walk_count+1 >= 27:
-        walk_count = 0
-
-    if left:
-        win.blit(walkLeft[walk_count//3], (x,y))
-        walk_count += 1
-    elif right:
-        win.blit(walkRight[walk_count//3], (x,y))
-        walk_count += 1
-    else:
-        win.blit(char, (x,y))
-
+    character.draw(win)
     pygame.display.update() 
 
-# # # # # # # 
-#main loops
+# # # # #
+#main loop
+character = player(300,410, 64, 64)
 run = True
+#
 while run:
     clock.tick(27)
-# # # # #
-    for event in pygame.event.get(): #creates/gets a list of all the events that happen
-        if event.type == pygame.QUIT: #if the big red button in the corner is pushed...
-            run = False
 # # #
-#list of...
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            run = False
+    # #
     keys = pygame.key.get_pressed()
-    # #
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-        left = True 
-        Right = False
-    elif keys[pygame.K_RIGHT] and x < (screenwidth-width): #'-vel' ?
-        x += vel
-        right = True
-        left = False
+    #
+    if keys[pygame.K_LEFT] and character.x > character.vel:
+        character.x -= character.vel
+        character.left = True 
+        character.Right = False
+    elif keys[pygame.K_RIGHT] and character.x < (screenwidth-character.width): #-character.vel / -vel
+        character.x += character.vel
+        character.right = True
+        character.left = False
     else:
-        right = False
-        left = False
-        walk_count = 0
-    # #
-    if not  (isJump) :
+        character.right = False
+        character.left = False
+        character.walk_count = 0
+        # #
+    if not (character.isJump) :
         if keys[pygame.K_SPACE]:
-            isJump = True
-            right = False
-            left = False
-            walk_count = 0
+            character.isJump = True
+            character.right = False
+            character.left = False
+            character.walk_count = 0
     else:
-        if JumpCount >= -10:
+        if character.JumpCount >= -10:
             neg = 1
-            if JumpCount < 0:
+            if character.JumpCount < 0:
                 neg = -1
-            y -= (JumpCount**2)/2*neg
-            JumpCount -= 1
+            character.y -= (character.JumpCount**2)/2*neg
+            character.JumpCount -= 1
         else:
-            isJump = False
-            JumpCount = 10
-    # #
+            character.isJump = False
+            character.JumpCount = 10
     redrawGameWindow()
 
-# # #
-
+# # # # # # #
 # # # # # # #
 pygame.quit()
